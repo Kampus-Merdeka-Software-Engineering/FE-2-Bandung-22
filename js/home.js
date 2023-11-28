8716be1583ced565656c83be607ef13d954bc2ab
Dilaau <nordilaaulia1904@gmail.com>
2023-11-28 22:58:52 +0800

    initial commit

--- js/home.js
+++ js/home.js
@@ -1,17 +1,178 @@
-//toggle class active
-const navbarNav = document.querySelector(".navbar-nav");
+// const navbarNav = document.querySelector(".navbar-nav");
 
-//hamburger di klik
+// document.querySelector("#hamburger-menu").onclick = () => {
+//   navbarNav.classList.toggle("active");
+// };
+
+// const hamburger = document.querySelector("#hamburger-menu");
+
+// document.addEventListener("click", function (e) {
+//   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+//     navbarNav.classList.remove("active");
+//   }
+// });
+
+// const shoppingCart = document.querySelector(".shopping-cart");
+// document.querySelector("#shopping-cart-button").onclick = (e) => {
+//   shoppingCart.classList.toggle("active");
+//   e.preventDefault();
+// };
+
+// const hm = document.querySelector("#hamburger-menu");
+// const sc = document.querySelector("#shopping-cart-button");
+
+// document.addEventListener("click", function (e) {
+//   if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+//     navbarNav.classList.remove("active");
+//   }
+//   if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+//     shoppingCart.classList.remove("active");
+//   }
+// });
+
+const navbarNav = document.querySelector(".navbar-nav");
+const hamburger = document.querySelector("#hamburger-menu");
+const shoppingCart = document.querySelector(".shopping-cart");
+const shoppingCartButton = document.querySelector("#shopping-cart-button");
+
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
-
-//luar sidebar untuk hilangkan nav
-
-const hamburger = document.querySelector("#hamburger-menu");
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+
+  if (
+    !shoppingCartButton.contains(e.target) &&
+    !shoppingCart.contains(e.target)
+  ) {
+    shoppingCart.classList.remove("active");
+  }
 });
+
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
+// Prevent closing shopping cart when clicking inside it
+shoppingCart.addEventListener("click", function (e) {
+  e.stopPropagation(); // Prevent the click event from reaching the document
+});
+
+document.addEventListener("click", function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
+});
+
+// New functions for close and checkout buttons
+function closeShoppingCart() {
+  shoppingCart.classList.remove("active");
+}
+
+function checkout() {
+  // Add your checkout logic here
+  alert("Checkout logic goes here!");
+  // You can redirect to a checkout page or perform other checkout actions
+}
+
+const wrapper = document.querySelector(".wrapper");
+const carousel = document.querySelector(".carousel");
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const arrowBtns = document.querySelectorAll(".wrapper i");
+const carouselChildrens = [...carousel.children];
+
+let isDragging = false,
+  isAutoPlay = true,
+  startX,
+  startScrollLeft,
+  timeoutId;
+
+// Get the number of cards that can fit in the carousel at once
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+// Insert copies of the last few cards to beginning of carousel for infinite scrolling
+carouselChildrens
+  .slice(-cardPerView)
+  .reverse()
+  .forEach((card) => {
+    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+  });
+
+// Insert copies of the first few cards to end of carousel for infinite scrolling
+carouselChildrens.slice(0, cardPerView).forEach((card) => {
+  carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+});
+
+// Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
+carousel.classList.add("no-transition");
+carousel.scrollLeft = carousel.offsetWidth;
+carousel.classList.remove("no-transition");
+
+// Add event listeners for the arrow buttons to scroll the carousel left and right
+arrowBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
+  });
+});
+
+const dragStart = (e) => {
+  isDragging = true;
+  carousel.classList.add("dragging");
+  // Records the initial cursor and scroll position of the carousel
+  startX = e.pageX;
+  startScrollLeft = carousel.scrollLeft;
+};
+
+const dragging = (e) => {
+  if (!isDragging) return; // if isDragging is false return from here
+  // Updates the scroll position of the carousel based on the cursor movement
+  carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+};
+
+const dragStop = () => {
+  isDragging = false;
+  carousel.classList.remove("dragging");
+};
+
+const infiniteScroll = () => {
+  // If the carousel is at the beginning, scroll to the end
+  if (carousel.scrollLeft === 0) {
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.scrollWidth - 2 * carousel.offsetWidth;
+    carousel.classList.remove("no-transition");
+  }
+  // If the carousel is at the end, scroll to the beginning
+  else if (
+    Math.ceil(carousel.scrollLeft) ===
+    carousel.scrollWidth - carousel.offsetWidth
+  ) {
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.offsetWidth;
+    carousel.classList.remove("no-transition");
+  }
+
+  // Clear existing timeout & start autoplay if mouse is not hovering over carousel
+  clearTimeout(timeoutId);
+  if (!wrapper.matches(":hover")) autoPlay();
+};
+
+const autoPlay = () => {
+  if (window.innerWidth < 800 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
+  // Autoplay the carousel after every 2500 ms
+  timeoutId = setTimeout(() => (carousel.scrollLeft += firstCardWidth), 1500);
+};
+autoPlay();
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
+wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+wrapper.addEventListener("mouseleave", autoPlay);
---
+++ src/app.js
@@ -0,0 +1,200 @@
+let products = [];
+
+// Fetch products data
+async function fetchProducts() {
+  try {
+    const response = await fetch("products.json");
+    const data = await response.json();
+    products = data;
+    addDataToHTML();
+  } catch (error) {
+    console.error("Error fetching products:", error);
+  }
+}
+
+// Call the fetchProducts function
+fetchProducts();
+
+// Function to add product data to HTML
+function addDataToHTML() {
+  const foodMenuHTML = document.querySelector(".food-menu");
+
+  // Template for the menu heading
+  const menuHeadingTemplate = `
+    <div class="heading">
+      <h1>&mdash; MENU &mdash;</h1>
+      <h7>Explore all flavors of coffee with us. There is always a new cup worth experiencing.</h7>
+    </div>
+  `;
+
+  foodMenuHTML.innerHTML = menuHeadingTemplate;
+
+  // Check if products data is available
+  if (products.length > 0) {
+    products.forEach((product) => {
+      const newProduct = createProductElement(product);
+      foodMenuHTML.appendChild(newProduct);
+    });
+  }
+}
+
+// Function to create a product element
+function createProductElement(product) {
+  const newProduct = document.createElement("div");
+  newProduct.classList.add("food-items");
+  newProduct.innerHTML = `
+    <img src="${product.image}" />
+    <div class="details">
+      <div class="details-sub">
+        <h5>${product.name}</h5>
+        <h5 class="price">$${product.price}</h5>
+      </div>
+      <p>${product.description}</p>
+      <button onclick="addCart(${product.id})">Add to Cart
+      </button>
+    </div>
+  `;
+  return newProduct;
+}
+
+let wishList = [];
+
+// Function to get wishList from cookies
+function checkCart() {
+  const cookieValue = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith("wishlist="));
+  if (cookieValue) {
+    wishList = JSON.parse(cookieValue.split("=")[1]);
+  }
+}
+
+// Call checkCart function
+checkCart();
+
+// Function to add a product to the cart
+function addCart(productId) {
+  const productCopy = JSON.parse(JSON.stringify(products));
+  const existingProduct = wishList.find((product) => product.id === productId);
+
+  if (!existingProduct) {
+    const dataProduct = productCopy.find((product) => product.id == productId);
+
+    if (dataProduct) {
+      dataProduct.quantity = 1;
+      wishList.push(dataProduct);
+    }
+  } else {
+    existingProduct.quantity++;
+  }
+
+  // Set cookie with updated wishList
+  const expirationDate = new Date(
+    "Thu, 31 Dec 2023 23:59:59 UTC"
+  ).toUTCString();
+  document.cookie = `wishlist=${JSON.stringify(
+    wishList
+  )}; expires=${expirationDate}; path=/`;
+
+  // Update HTML
+  addCartToHTML();
+}
+
+// Function to update the HTML with wishList items
+function addCartToHTML() {
+  const wishListHTML = document.querySelector(".wishlist");
+  wishListHTML.innerHTML = "";
+
+  let totalQuantity = 0;
+  let totalPrice = 0;
+
+  if (wishList.length === 0) {
+    // If wishlist is empty, display a message
+    wishListHTML.innerHTML = "<p>Your wishlist is empty.</p>";
+  } else {
+    wishList.forEach((product) => {
+      if (product) {
+        const newCart = document.createElement("div");
+        newCart.classList.add("item");
+        newCart.innerHTML = `
+          <img src="${product.image}" alt="${product.name}" />
+          <div class="content">
+            <div class="name">${product.name}</div>
+            <div class="price">$${product.price * product.quantity}</div>
+            <div class="quantity">
+              <button onclick="removeItem(${product.id})" class="btn">-</button>
+              <span class="value">${product.quantity}</span>
+              <button onclick="addItem(${product.id})" class="btn">+</button>
+              <button onclick="removeItemFromWishlist(${
+                product.id
+              })" class="btn">&#128465;</button>
+            </div>
+          </div>
+        `;
+        wishListHTML.appendChild(newCart);
+        totalQuantity += product.quantity;
+        totalPrice += product.price * product.quantity;
+      }
+    });
+  }
+
+  // Update total quantity and total price in the HTML
+  const totalHTML = document.querySelector(".totalQuantity");
+  totalHTML.textContent = totalQuantity;
+
+  const totalPriceHTML = document.querySelector(".totalPrice");
+  totalPriceHTML.textContent = `$${totalPrice}`;
+}
+
+// Function to remove one quantity of an item from the wishlist
+function removeItem(productId) {
+  const existingProduct = wishList.find((product) => product.id === productId);
+  if (existingProduct) {
+    existingProduct.quantity--;
+
+    if (existingProduct.quantity === 0) {
+      // If quantity becomes zero, remove the item from the wishlist
+      removeItemFromWishlist(productId);
+    } else {
+      // Update cookie and HTML
+      updateWishlist();
+    }
+  }
+}
+
+// Function to remove an item from the cart and wishlist
+function removeItemFromWishlist(productId) {
+  const productIndex = wishList.findIndex(
+    (product) => product.id === productId
+  );
+  if (productIndex !== -1) {
+    wishList.splice(productIndex, 1);
+    // Update cookie and HTML
+    updateWishlist();
+  }
+}
+
+// Function to add an item to the cart
+function addItem(productId) {
+  const existingProduct = wishList.find((product) => product.id === productId);
+  if (existingProduct) {
+    existingProduct.quantity++;
+
+    // Update cookie and HTML
+    updateWishlist();
+  }
+}
+
+// Function to update wishlist in the cookie and HTML
+function updateWishlist() {
+  const expirationDate = new Date(
+    "Thu, 31 Dec 2023 23:59:59 UTC"
+  ).toUTCString();
+  document.cookie = `wishlist=${JSON.stringify(
+    wishList
+  )}; expires=${expirationDate}; path=/`;
+  addCartToHTML();
+}
+
+// Initial call to update HTML
+addCartToHTML();
